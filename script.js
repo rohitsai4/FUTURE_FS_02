444d14fb40b62f2e82f643897e7d8495bacaa860
rohitsai4 <alugolurohit9@gmail.com>
2026-06-07 11:34:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+```javascript
+const REPORT_SURVEY_ID = "019a46ef-f7dd-0000-58be-14fbb8b91d15";
+const MESSAGE_ID = "019e95c4-ecf2-7999-b2b5-885d313e3106";
+const EVALUATION_SESSION_ID = "019e8b3e-2380-76ce-a009-e69af93b8925";
+
+const iframe = document.getElementById("preview-iframe");
+
+iframe.addEventListener("load", () => {
+    iframe.focus();
+});
+```
